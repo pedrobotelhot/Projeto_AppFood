3e095bc1074dc76895b100607031ccdb1dcb50e3
Pedro Henrique Botelho <54961750+pedrobotelhot@users.noreply.github.com>
2022-12-28 08:04:31 -0300

    AppFood

---
+++ AppFood/app/src/main/java/food/com/br/appfood/MainActivity.java
@@ -0,0 +1,14 @@
+package food.com.br.appfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
